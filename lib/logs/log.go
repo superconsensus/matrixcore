bcedfa5b0ce3f6bf163424dcfc0f0787ee8c993b
fanbingxin <fanbingxin.me@gmail.com>
2021-08-26 20:05:52 +0800

    logs: use buffered file handler (#231)
    (cherry picked from commit f67349c44c1537e6d58f0577ade0ba3945d0e77e)

--- lib/logs/log.go
+++ lib/logs/log.go
@@ -63,9 +63,9 @@ func OpenLog(lc *lconf.LogConf, logDir string) (LogDriver, error) {
 	// only valid if `RotateInterval` and `RotateBackups` greater than 0
 	var nmHandler, wfHandler log.Handler
 	if lc.RotateInterval > 0 && lc.RotateBackups > 0 {
-		nmHandler = log.Must.RotateFileHandler(
+		nmHandler = mustBufferFileHandler(
 			infoFile, lfmt, lc.RotateInterval, lc.RotateBackups)
-		wfHandler = log.Must.RotateFileHandler(
+		wfHandler = mustBufferFileHandler(
 			wfFile, lfmt, lc.RotateInterval, lc.RotateBackups)
 	} else {
 		nmHandler = log.Must.FileHandler(infoFile, lfmt)
@@ -78,16 +78,16 @@ func OpenLog(lc *lconf.LogConf, logDir string) (LogDriver, error) {
 	}
 
 	// prints log level between `lvLevel` to Info to common log
-	nmfileh := log.BoundLvlFilterHandler(lvLevel, log.LvlInfo, nmHandler)
+	nmfileh := log.LvlFilterHandler(lvLevel, nmHandler)
 	// prints log level greater or equal to Warn to wf log
 	wffileh := log.LvlFilterHandler(log.LvlWarn, wfHandler)
 
 	var lhd log.Handler
 	if lc.Console {
 		hstd := log.StreamHandler(os.Stderr, lfmt)
-		lhd = log.SyncHandler(log.MultiHandler(hstd, nmfileh, wffileh))
+		lhd = log.MultiHandler(hstd, nmfileh, wffileh)
 	} else {
-		lhd = log.SyncHandler(log.MultiHandler(nmfileh, wffileh))
+		lhd = log.MultiHandler(nmfileh, wffileh)
 	}
 	xlog.SetHandler(lhd)
 

OpenAtomFoundation/xupercore

consensus: add mutex in smr & qcPendingTree. (#232)

kernel/consensus/base/common/common.go
@@ -1,18 +1,15 @@
 package utils
 
 import (
-	"container/list"
 	"encoding/json"
 	"errors"
 	"time"
 
 	"github.com/golang/protobuf/proto"
 	lpb "github.com/xuperchain/xupercore/bcs/ledger/xledger/xldgpb"
-	chainedBft "github.com/xuperchain/xupercore/kernel/consensus/base/driver/chained-bft"
 	bftPb "github.com/xuperchain/xupercore/kernel/consensus/base/driver/chained-bft/pb"
-	cctx "github.com/xuperchain/xupercore/kernel/consensus/context"
+	bftStorage "github.com/xuperchain/xupercore/kernel/consensus/base/driver/chained-bft/storage"
 	"github.com/xuperchain/xupercore/kernel/contract"
-	"github.com/xuperchain/xupercore/lib/logs"
 )
 
 var (
@@ -56,114 +53,6 @@ func AddressEqual(a []string, b []string) bool {
 	return true
 }
 
-// initQCTree 创建了smr需要的QC树存储，该Tree存储了目前待commit的QC信息
-func InitQCTree(startHeight int64, ledger cctx.LedgerRely, log logs.Logger) *chainedBft.QCPendingTree {
-	// 初始状态应该是start高度的前一个区块为genesisQC，即tipBlock
-	g, err := ledger.QueryBlockByHeight(startHeight - 1)
-	if err != nil {
-		return nil
-	}
-	gQC := &chainedBft.QuorumCert{
-		VoteInfo: &chainedBft.VoteInfo{
-			ProposalId:   g.GetBlockid(),
-			ProposalView: g.GetHeight(),
-		},
-		LedgerCommitInfo: &chainedBft.LedgerCommitInfo{
-			CommitStateId: g.GetBlockid(),
-		},
-	}
-	gNode := &chainedBft.ProposalNode{
-		In: gQC,
-	}
-	tip := ledger.GetTipBlock()
-	// 当前为初始状态
-	if tip.GetHeight() <= startHeight {
-		return &chainedBft.QCPendingTree{
-			Genesis:    gNode,
-			Root:       gNode,
-			HighQC:     gNode,
-			CommitQC:   gNode,
-			Log:        log,
-			OrphanList: list.New(),
-			OrphanMap:  make(map[string]bool),
-		}
-	}
-	// 重启状态时将root->tipBlock-3, generic->tipBlock-2, highQC->tipBlock-1
-	// 若tipBlock<=2, root->genesisBlock, highQC->tipBlock-1
-	tipNode := makeTreeNode(ledger, tip.GetHeight())
-	if tip.GetHeight() < 3 {
-		tree := &chainedBft.QCPendingTree{
-			Genesis:    gNode,
-			Root:       makeTreeNode(ledger, 0),
-			Log:        log,
-			OrphanList: list.New(),
-			OrphanMap:  make(map[string]bool),
-		}
-		switch tip.GetHeight() {
-		case 0:
-			tree.HighQC = tree.Root
-			return tree
-		case 1:
-			tree.HighQC = tree.Root
-			tree.HighQC.Sons = append(tree.HighQC.Sons, tipNode)
-			return tree
-		case 2:
-			tree.HighQC = makeTreeNode(ledger, 1)
-			tree.HighQC.Sons = append(tree.HighQC.Sons, tipNode)
-			tree.Root.Sons = append(tree.Root.Sons, tree.HighQC)
-		}
-		return tree
-	}
-	tree := &chainedBft.QCPendingTree{
-		Genesis:    gNode,
-		Root:       makeTreeNode(ledger, tip.GetHeight()-3),
-		GenericQC:  makeTreeNode(ledger, tip.GetHeight()-2),
-		HighQC:     makeTreeNode(ledger, tip.GetHeight()-1),
-		Log:        log,
-		OrphanList: list.New(),
-		OrphanMap:  make(map[string]bool),
-	}
-	// 手动组装Tree结构
-	tree.Root.Sons = append(tree.Root.Sons, tree.GenericQC)
-	tree.GenericQC.Sons = append(tree.GenericQC.Sons, tree.HighQC)
-	tree.HighQC.Sons = append(tree.HighQC.Sons, tipNode)
-	return tree
-}
-
-func makeTreeNode(ledger cctx.LedgerRely, height int64) *chainedBft.ProposalNode {
-	b, err := ledger.QueryBlockByHeight(height)
-	if err != nil {
-		return nil
-	}
-	pre, err := ledger.QueryBlockByHeight(height - 1)
-	if err != nil {
-		return &chainedBft.ProposalNode{
-			In: &chainedBft.QuorumCert{
-				VoteInfo: &chainedBft.VoteInfo{
-					ProposalId:   b.GetBlockid(),
-					ProposalView: b.GetHeight(),
-				},
-				LedgerCommitInfo: &chainedBft.LedgerCommitInfo{
-					CommitStateId: b.GetBlockid(),
-				},
-			},
-		}
-	}
-	return &chainedBft.ProposalNode{
-		In: &chainedBft.QuorumCert{
-			VoteInfo: &chainedBft.VoteInfo{
-				ProposalId:   b.GetBlockid(),
-				ProposalView: b.GetHeight(),
-				ParentId:     pre.GetBlockid(),
-				ParentView:   pre.GetHeight(),
-			},
-			LedgerCommitInfo: &chainedBft.LedgerCommitInfo{
-				CommitStateId: b.GetBlockid(),
-			},
-		},
-	}
-}
-
 func CleanProduceMap(isProduce map[int64]bool, period int64) {
 	// 删除已经落盘的所有key
 	if len(isProduce) <= MaxMapSize {
@@ -199,7 +88,7 @@ func ParseOldQCStorage(storage []byte) (*ConsensusStorage, error) {
 }
 
 // OldQCToNew 为老的QC pb结构转化为新的QC结构
-func OldQCToNew(storage []byte) (*chainedBft.QuorumCert, error) {
+func OldQCToNew(storage []byte) (bftStorage.QuorumCertInterface, error) {
 	oldS, err := ParseOldQCStorage(storage)
 	if err != nil {
 		return nil, err
@@ -214,21 +103,18 @@ func OldQCToNew(storage []byte) (*chainedBft.QuorumCert, error) {
 	if err != nil {
 		return nil, err
 	}
-	newQC := &chainedBft.QuorumCert{
-		VoteInfo: &chainedBft.VoteInfo{
+	newQC := bftStorage.NewQuorumCert(
+		&bftStorage.VoteInfo{
 			ProposalId:   oldQC.ProposalId,
 			ProposalView: oldQC.ViewNumber,
 			ParentId:     justifyQC.ProposalId,
 			ParentView:   justifyQC.ViewNumber,
-		},
-	}
-	SignInfos := OldSignToNew(storage)
-	newQC.SignInfos = SignInfos
+		}, nil, OldSignToNew(storage))
 	return newQC, nil
 }
 
 // NewToOldQC 为新的QC pb结构转化为老pb结构
-func NewToOldQC(new *chainedBft.QuorumCert) (*lpb.QuorumCert, error) {
+func NewToOldQC(new *bftStorage.QuorumCert) (*lpb.QuorumCert, error) {
 	oldParentQC := &lpb.QuorumCert{
 		ProposalId: new.VoteInfo.ParentId,
 		ViewNumber: new.VoteInfo.ParentView,

kernel/consensus/base/driver/chained-bft/mock/main_test.go
@@ -1,0 +1,131 @@
+package mock
+
+import (
+	"bytes"
+	"testing"
+
+	"github.com/xuperchain/xupercore/kernel/consensus/base/driver/chained-bft/storage"
+)
+
+func TestUpdateHighQC(t *testing.T) {
+	tree := PrepareTree(t)
+	id2 := []byte{3}
+	tree.UpdateHighQC(id2)
+	if tree.GetHighQC().In.GetProposalView() != 2 {
+		t.Error("TestUpdateHighQC update highQC error", "height", tree.GetHighQC().In.GetProposalView())
+		return
+	}
+	if tree.GetGenericQC().In.GetProposalView() != 1 {
+		t.Error("TestUpdateHighQC update genericQC error", "height", tree.GetGenericQC().In.GetProposalView())
+		return
+	}
+	if tree.GetLockedQC().In.GetProposalView() != 0 {
+		t.Error("TestUpdateHighQC update lockedQC error", "height", tree.GetLockedQC().In.GetProposalView())
+		return
+	}
+}
+
+func TestEnforceUpdateHighQC(t *testing.T) {
+	tree := PrepareTree(t)
+	tree.UpdateHighQC([]byte{3})
+	tree.EnforceUpdateHighQC([]byte{1})
+	if tree.GetHighQC().In.GetProposalView() != 1 {
+		t.Error("enforceUpdateHighQC update highQC error", "height", tree.GetHighQC().In.GetProposalView())
+		return
+	}
+}
+
+// TestUpdateCommit Tree如下
+// root 0
+// |    \
+// node1 1 node12
+// |
+// node2 2
+// |
+// node3 3
+// |
+// node4 4
+func TestUpdateCommit(t *testing.T) {
+	tree := PrepareTree(t)
+	tree.UpdateHighQC([]byte{3})
+
+	QC3 := MockCreateQC([]byte{4}, 3, []byte{3}, 2)
+	node1 := &storage.ProposalNode{
+		In: QC3,
+	}
+	if err := tree.UpdateQcStatus(node1); err != nil {
+		t.Error("updateQcStatus error")
+		return
+	}
+	QC4 := MockCreateQC([]byte{5}, 4, []byte{4}, 3)
+	node2 := &storage.ProposalNode{
+		In: QC4,
+	}
+	if err := tree.UpdateQcStatus(node2); err != nil {
+		t.Error("updateQcStatus node2 error")
+		return
+	}
+	tree.UpdateCommit([]byte{5})
+	if tree.GetRootQC().In.GetProposalView() != 1 {
+		t.Error("updateCommit error")
+	}
+}
+
+// TestDFSQueryNode Tree如下
+//           --------------------root ([]byte{0}, 0)-----------------------
+//            		  |       |                          |
+// (([]byte{1}, 1)) node1 node12 ([]byte{2}, 1) orphan4<[]byte{10}, 1>
+//                    |                                  |            \
+//  ([]byte{3}, 2)  node2				        orphan2<[]byte{30}, 2> orphan3<[]byte{35}, 2>
+//														 |
+// 												orphan1<[]byte{40}, 3>
+//
+func TestInsertOrphan(t *testing.T) {
+	tree := PrepareTree(t)
+	orphan1 := &storage.ProposalNode{
+		In: MockCreateQC([]byte{40}, 3, []byte{30}, 2),
+	}
+	tree.UpdateQcStatus(orphan1)
+	orphan := tree.MockGetOrphan()
+	e1 := orphan.Front()
+	o1, ok := e1.Value.(*storage.ProposalNode)
+	if !ok {
+		t.Error("OrphanList type error1!")
+	}
+	if o1.In.GetProposalView() != 3 {
+		t.Error("OrphanList insert error!")
+	}
+	orphan2 := &storage.ProposalNode{
+		In: MockCreateQC([]byte{30}, 2, []byte{10}, 1),
+	}
+	tree.UpdateQcStatus(orphan2)
+	e1 = orphan.Front()
+	o1, ok = e1.Value.(*storage.ProposalNode)
+	if !ok {
+		t.Error("OrphanList type error2!")
+	}
+	if !bytes.Equal(o1.In.GetProposalId(), []byte{30}) {
+		t.Error("OrphanList insert error2!", "id", o1.In.GetProposalId())
+	}
+	orphan3 := &storage.ProposalNode{
+		In: MockCreateQC([]byte{35}, 2, []byte{10}, 1),
+	}
+	tree.UpdateQcStatus(orphan3)
+	e1 = orphan.Front()
+	o1, _ = e1.Value.(*storage.ProposalNode)
+	e2 := e1.Next()
+	o2, _ := e2.Value.(*storage.ProposalNode)
+	if !bytes.Equal(o1.In.GetProposalId(), []byte{30}) {
+		t.Error("OrphanList insert error3!", "id", o1.In.GetProposalId())
+	}
+	if !bytes.Equal(o2.In.GetProposalId(), []byte{35}) {
+		t.Error("OrphanList insert error4!", "id", o1.In.GetProposalId())
+	}
+	orphan4 := &storage.ProposalNode{
+		In: MockCreateQC([]byte{10}, 1, []byte{0}, 0),
+	}
+	tree.UpdateQcStatus(orphan4)
+	if orphan.Len() != 0 {
+		t.Error("OrphanList adopt error!")
+	}
+}

kernel/consensus/base/driver/chained-bft/pacemaker.go
@@ -1,5 +1,9 @@
 package chained_bft
 
+import (
+	"github.com/xuperchain/xupercore/kernel/consensus/base/driver/chained-bft/storage"
+)
+
 // PacemakerInterface is the interface of Pacemaker. It responsible for generating a new round.
 // We assume Pacemaker in all correct replicas will have synchronized leadership after GST.
 // Safty is entirely decoupled from liveness by any potential instantiation of Packmaker.
@@ -8,7 +12,7 @@ type PacemakerInterface interface {
 	// CurrentView return current view of this node.
 	GetCurrentView() int64
 	// 原NextNewProposal，generate new proposal directly.
-	AdvanceView(qc QuorumCertInterface) (bool, error)
+	AdvanceView(qc storage.QuorumCertInterface) (bool, error)
 }
 
 // DefaultPaceMaker 是一个PacemakerInterface的默认实现，我们与PacemakerInterface放置在一起，方便查看
@@ -20,7 +24,7 @@ type DefaultPaceMaker struct {
 	// timeout int64
 }
 
-func (p *DefaultPaceMaker) AdvanceView(qc QuorumCertInterface) (bool, error) {
+func (p *DefaultPaceMaker) AdvanceView(qc storage.QuorumCertInterface) (bool, error) {
 	r := qc.GetProposalView()
 	if r+1 > p.CurrentView {
 		p.CurrentView = r + 1

kernel/consensus/base/driver/chained-bft/pacemaker_test.go
@@ -2,14 +2,16 @@ package chained_bft
 
 import (
 	"testing"
+
+	"github.com/xuperchain/xupercore/kernel/consensus/base/driver/chained-bft/storage"
 )
 
 func TestPaceMaker(t *testing.T) {
 	p := &DefaultPaceMaker{
 		CurrentView: 0,
 	}
-	qc := &QuorumCert{
-		VoteInfo: &VoteInfo{
+	qc := &storage.QuorumCert{
+		VoteInfo: &storage.VoteInfo{
 			ProposalId:   []byte{1},
 			ProposalView: 1,
 		},

kernel/consensus/base/driver/chained-bft/saftyrules_test.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 
 	cCrypto "github.com/xuperchain/xupercore/kernel/consensus/base/driver/chained-bft/crypto"
+	"github.com/xuperchain/xupercore/kernel/consensus/base/driver/chained-bft/mock"
 	chainedBftPb "github.com/xuperchain/xupercore/kernel/consensus/base/driver/chained-bft/pb"
 )
 
@@ -46,42 +47,32 @@ func TestIsInSlice(t *testing.T) {
 }
 
 func TestCheckProposal(t *testing.T) {
+	th, _ := mock.NewTestHelper()
+	defer th.Close()
 	s := &DefaultSaftyRules{
 		lastVoteRound:  0,
 		preferredRound: 0,
-		QcTree:         initQcTree(),
-		Log:            NewFakeLogger("nodeA"),
+		QcTree:         mock.MockInitQcTree(),
+		Log:            th.Log,
 	}
 	a, cc := NewFakeCryptoClient("nodeA", t)
 	s.Crypto = &cCrypto.CBFTCrypto{
 		Address:      &a,
 		CryptoClient: cc,
 	}
-	generic := CreateQC([]byte{1}, 1, []byte{0}, 01)
+	generic := mock.MockCreateQC([]byte{1}, 1, []byte{0}, 01)
 	msg := &chainedBftPb.ProposalMsg{
 		ProposalView: 1,
 		ProposalId:   []byte{1},
 	}
-	r, err := s.Crypto.SignProposalMsg(msg)
-	if err != nil {
-		t.Error("SignProposalMsg error")
-		return
-	}
-	generic.SignInfos = []*chainedBftPb.QuorumCertSign{r.Sign}
-	node1 := &ProposalNode{
-		In: generic,
-	}
-	if err := s.QcTree.updateQcStatus(node1); err != nil {
+	r, _ := s.Crypto.SignProposalMsg(msg)
+	node1 := mock.MockCreateNode(generic, []*chainedBftPb.QuorumCertSign{r.Sign})
+	if err := s.QcTree.UpdateQcStatus(node1); err != nil {
 		t.Error("TestUpdateQcStatus empty parent error")
 		return
 	}
-	proposal := CreateQC([]byte{2}, 2, []byte{1}, 1)
-	proposal.VoteInfo.ParentId = []byte{1}
+	proposal := mock.MockCreateQC([]byte{2}, 2, []byte{1}, 1)
 	s.CheckProposal(proposal, generic, []string{"gNhga8vLc4JcmoHB2yeef2adBhntkc5d1"})
 	s.VoteProposal([]byte{2}, 2, generic)
 	s.CheckVote(generic, "123", []string{"gNhga8vLc4JcmoHB2yeef2adBhntkc5d1"})
-}
-
-func TestCheckVote(t *testing.T) {
-
 }

kernel/consensus/base/driver/chained-bft/storage/quorum_cert_impl.go
@@ -1,0 +1,35 @@
+package storage
+
+import (
+	pb "github.com/xuperchain/xupercore/kernel/consensus/base/driver/chained-bft/pb"
+)
+
+// quorumCert 是HotStuff的基础结构，它表示了一个节点本地状态以及其余节点对该状态的确认
+type QuorumCert struct {
+	// 本次qc的vote对象，该对象中嵌入了上次的QCid，因此删除原有的ProposalMsg部分
+	VoteInfo *VoteInfo
+	// 当前本地账本的状态
+	LedgerCommitInfo *LedgerCommitInfo
+	// SignInfos is the signs of the leader gathered from replicas of a specifically certType.
+	SignInfos []*pb.QuorumCertSign
+}
+
+func (qc *QuorumCert) GetProposalView() int64 {
+	return qc.VoteInfo.ProposalView
+}
+
+func (qc *QuorumCert) GetProposalId() []byte {
+	return qc.VoteInfo.ProposalId
+}
+
+func (qc *QuorumCert) GetParentProposalId() []byte {
+	return qc.VoteInfo.ParentId
+}
+
+func (qc *QuorumCert) GetParentView() int64 {
+	return qc.VoteInfo.ParentView
+}
+
+func (qc *QuorumCert) GetSignsInfo() []*pb.QuorumCertSign {
+	return qc.SignInfos
+}

superconsensus/matrixcore

(cherry picked from commit f349acf51a39572eead3c03faf58ef488d064036)

kernel/consensus/base/common/common.go
@@ -1,18 +1,15 @@
 package utils
 
 import (
-	"container/list"
 	"encoding/json"
 	"errors"
 	"time"
 
 	"github.com/golang/protobuf/proto"
 	lpb "github.com/superconsensus/matrixcore/bcs/ledger/xledger/xldgpb"
-	chainedBft "github.com/superconsensus/matrixcore/kernel/consensus/base/driver/chained-bft"
 	bftPb "github.com/superconsensus/matrixcore/kernel/consensus/base/driver/chained-bft/pb"
-	cctx "github.com/superconsensus/matrixcore/kernel/consensus/context"
 	"github.com/superconsensus/matrixcore/kernel/contract"
-	"github.com/superconsensus/matrixcore/lib/logs"
+	bftStorage "github.com/superconsensus/matrixcore/kernel/consensus/base/driver/chained-bft/storage"
 )
 
 var (
@@ -56,114 +53,6 @@ func AddressEqual(a []string, b []string) bool {
 	return true
 }
 
-// initQCTree 创建了smr需要的QC树存储，该Tree存储了目前待commit的QC信息
-func InitQCTree(startHeight int64, ledger cctx.LedgerRely, log logs.Logger) *chainedBft.QCPendingTree {
-	// 初始状态应该是start高度的前一个区块为genesisQC，即tipBlock
-	g, err := ledger.QueryBlockByHeight(startHeight - 1)
-	if err != nil {
-		return nil
-	}
-	gQC := &chainedBft.QuorumCert{
-		VoteInfo: &chainedBft.VoteInfo{
-			ProposalId:   g.GetBlockid(),
-			ProposalView: g.GetHeight(),
-		},
-		LedgerCommitInfo: &chainedBft.LedgerCommitInfo{
-			CommitStateId: g.GetBlockid(),
-		},
-	}
-	gNode := &chainedBft.ProposalNode{
-		In: gQC,
-	}
-	tip := ledger.GetTipBlock()
-	// 当前为初始状态
-	if tip.GetHeight() <= startHeight {
-		return &chainedBft.QCPendingTree{
-			Genesis:    gNode,
-			Root:       gNode,
-			HighQC:     gNode,
-			CommitQC:   gNode,
-			Log:        log,
-			OrphanList: list.New(),
-			OrphanMap:  make(map[string]bool),
-		}
-	}
-	// 重启状态时将root->tipBlock-3, generic->tipBlock-2, highQC->tipBlock-1
-	// 若tipBlock<=2, root->genesisBlock, highQC->tipBlock-1
-	tipNode := makeTreeNode(ledger, tip.GetHeight())
-	if tip.GetHeight() < 3 {
-		tree := &chainedBft.QCPendingTree{
-			Genesis:    gNode,
-			Root:       makeTreeNode(ledger, 0),
-			Log:        log,
-			OrphanList: list.New(),
-			OrphanMap:  make(map[string]bool),
-		}
-		switch tip.GetHeight() {
-		case 0:
-			tree.HighQC = tree.Root
-			return tree
-		case 1:
-			tree.HighQC = tree.Root
-			tree.HighQC.Sons = append(tree.HighQC.Sons, tipNode)
-			return tree
-		case 2:
-			tree.HighQC = makeTreeNode(ledger, 1)
-			tree.HighQC.Sons = append(tree.HighQC.Sons, tipNode)
-			tree.Root.Sons = append(tree.Root.Sons, tree.HighQC)
-		}
-		return tree
-	}
-	tree := &chainedBft.QCPendingTree{
-		Genesis:    gNode,
-		Root:       makeTreeNode(ledger, tip.GetHeight()-3),
-		GenericQC:  makeTreeNode(ledger, tip.GetHeight()-2),
-		HighQC:     makeTreeNode(ledger, tip.GetHeight()-1),
-		Log:        log,
-		OrphanList: list.New(),
-		OrphanMap:  make(map[string]bool),
-	}
-	// 手动组装Tree结构
-	tree.Root.Sons = append(tree.Root.Sons, tree.GenericQC)
-	tree.GenericQC.Sons = append(tree.GenericQC.Sons, tree.HighQC)
-	tree.HighQC.Sons = append(tree.HighQC.Sons, tipNode)
-	return tree
-}
-
-func makeTreeNode(ledger cctx.LedgerRely, height int64) *chainedBft.ProposalNode {
-	b, err := ledger.QueryBlockByHeight(height)
-	if err != nil {
-		return nil
-	}
-	pre, err := ledger.QueryBlockByHeight(height - 1)
-	if err != nil {
-		return &chainedBft.ProposalNode{
-			In: &chainedBft.QuorumCert{
-				VoteInfo: &chainedBft.VoteInfo{
-					ProposalId:   b.GetBlockid(),
-					ProposalView: b.GetHeight(),
-				},
-				LedgerCommitInfo: &chainedBft.LedgerCommitInfo{
-					CommitStateId: b.GetBlockid(),
-				},
-			},
-		}
-	}
-	return &chainedBft.ProposalNode{
-		In: &chainedBft.QuorumCert{
-			VoteInfo: &chainedBft.VoteInfo{
-				ProposalId:   b.GetBlockid(),
-				ProposalView: b.GetHeight(),
-				ParentId:     pre.GetBlockid(),
-				ParentView:   pre.GetHeight(),
-			},
-			LedgerCommitInfo: &chainedBft.LedgerCommitInfo{
-				CommitStateId: b.GetBlockid(),
-			},
-		},
-	}
-}
-
 func CleanProduceMap(isProduce map[int64]bool, period int64) {
 	// 删除已经落盘的所有key
 	if len(isProduce) <= MaxMapSize {
@@ -199,7 +88,7 @@ func ParseOldQCStorage(storage []byte) (*ConsensusStorage, error) {
 }
 
 // OldQCToNew 为老的QC pb结构转化为新的QC结构
-func OldQCToNew(storage []byte) (*chainedBft.QuorumCert, error) {
+func OldQCToNew(storage []byte) (bftStorage.QuorumCertInterface, error) {
 	oldS, err := ParseOldQCStorage(storage)
 	if err != nil {
 		return nil, err
@@ -214,21 +103,18 @@ func OldQCToNew(storage []byte) (*chainedBft.QuorumCert, error) {
 	if err != nil {
 		return nil, err
 	}
-	newQC := &chainedBft.QuorumCert{
-		VoteInfo: &chainedBft.VoteInfo{
+	newQC := bftStorage.NewQuorumCert(
+		&bftStorage.VoteInfo{
 			ProposalId:   oldQC.ProposalId,
 			ProposalView: oldQC.ViewNumber,
 			ParentId:     justifyQC.ProposalId,
 			ParentView:   justifyQC.ViewNumber,
-		},
-	}
-	SignInfos := OldSignToNew(storage)
-	newQC.SignInfos = SignInfos
+		}, nil, OldSignToNew(storage))
 	return newQC, nil
 }
 
 // NewToOldQC 为新的QC pb结构转化为老pb结构
-func NewToOldQC(new *chainedBft.QuorumCert) (*lpb.QuorumCert, error) {
+func NewToOldQC(new *bftStorage.QuorumCert) (*lpb.QuorumCert, error) {
 	oldParentQC := &lpb.QuorumCert{
 		ProposalId: new.VoteInfo.ParentId,
 		ViewNumber: new.VoteInfo.ParentView,

kernel/consensus/base/driver/chained-bft/mock/main_test.go
@@ -1,0 +1,131 @@
+package mock
+
+import (
+	"bytes"
+	"testing"
+
+	"github.com/superconsensus/matrixcore/kernel/consensus/base/driver/chained-bft/storage"
+)
+
+func TestUpdateHighQC(t *testing.T) {
+	tree := PrepareTree(t)
+	id2 := []byte{3}
+	tree.UpdateHighQC(id2)
+	if tree.GetHighQC().In.GetProposalView() != 2 {
+		t.Error("TestUpdateHighQC update highQC error", "height", tree.GetHighQC().In.GetProposalView())
+		return
+	}
+	if tree.GetGenericQC().In.GetProposalView() != 1 {
+		t.Error("TestUpdateHighQC update genericQC error", "height", tree.GetGenericQC().In.GetProposalView())
+		return
+	}
+	if tree.GetLockedQC().In.GetProposalView() != 0 {
+		t.Error("TestUpdateHighQC update lockedQC error", "height", tree.GetLockedQC().In.GetProposalView())
+		return
+	}
+}
+
+func TestEnforceUpdateHighQC(t *testing.T) {
+	tree := PrepareTree(t)
+	tree.UpdateHighQC([]byte{3})
+	tree.EnforceUpdateHighQC([]byte{1})
+	if tree.GetHighQC().In.GetProposalView() != 1 {
+		t.Error("enforceUpdateHighQC update highQC error", "height", tree.GetHighQC().In.GetProposalView())
+		return
+	}
+}
+
+// TestUpdateCommit Tree如下
+// root 0
+// |    \
+// node1 1 node12
+// |
+// node2 2
+// |
+// node3 3
+// |
+// node4 4
+func TestUpdateCommit(t *testing.T) {
+	tree := PrepareTree(t)
+	tree.UpdateHighQC([]byte{3})
+
+	QC3 := MockCreateQC([]byte{4}, 3, []byte{3}, 2)
+	node1 := &storage.ProposalNode{
+		In: QC3,
+	}
+	if err := tree.UpdateQcStatus(node1); err != nil {
+		t.Error("updateQcStatus error")
+		return
+	}
+	QC4 := MockCreateQC([]byte{5}, 4, []byte{4}, 3)
+	node2 := &storage.ProposalNode{
+		In: QC4,
+	}
+	if err := tree.UpdateQcStatus(node2); err != nil {
+		t.Error("updateQcStatus node2 error")
+		return
+	}
+	tree.UpdateCommit([]byte{5})
+	if tree.GetRootQC().In.GetProposalView() != 1 {
+		t.Error("updateCommit error")
+	}
+}
+
+// TestDFSQueryNode Tree如下
+//           --------------------root ([]byte{0}, 0)-----------------------
+//            		  |       |                          |
+// (([]byte{1}, 1)) node1 node12 ([]byte{2}, 1) orphan4<[]byte{10}, 1>
+//                    |                                  |            \
+//  ([]byte{3}, 2)  node2				        orphan2<[]byte{30}, 2> orphan3<[]byte{35}, 2>
+//														 |
+// 												orphan1<[]byte{40}, 3>
+//
+func TestInsertOrphan(t *testing.T) {
+	tree := PrepareTree(t)
+	orphan1 := &storage.ProposalNode{
+		In: MockCreateQC([]byte{40}, 3, []byte{30}, 2),
+	}
+	tree.UpdateQcStatus(orphan1)
+	orphan := tree.MockGetOrphan()
+	e1 := orphan.Front()
+	o1, ok := e1.Value.(*storage.ProposalNode)
+	if !ok {
+		t.Error("OrphanList type error1!")
+	}
+	if o1.In.GetProposalView() != 3 {
+		t.Error("OrphanList insert error!")
+	}
+	orphan2 := &storage.ProposalNode{
+		In: MockCreateQC([]byte{30}, 2, []byte{10}, 1),
+	}
+	tree.UpdateQcStatus(orphan2)
+	e1 = orphan.Front()
+	o1, ok = e1.Value.(*storage.ProposalNode)
+	if !ok {
+		t.Error("OrphanList type error2!")
+	}
+	if !bytes.Equal(o1.In.GetProposalId(), []byte{30}) {
+		t.Error("OrphanList insert error2!", "id", o1.In.GetProposalId())
+	}
+	orphan3 := &storage.ProposalNode{
+		In: MockCreateQC([]byte{35}, 2, []byte{10}, 1),
+	}
+	tree.UpdateQcStatus(orphan3)
+	e1 = orphan.Front()
+	o1, _ = e1.Value.(*storage.ProposalNode)
+	e2 := e1.Next()
+	o2, _ := e2.Value.(*storage.ProposalNode)
+	if !bytes.Equal(o1.In.GetProposalId(), []byte{30}) {
+		t.Error("OrphanList insert error3!", "id", o1.In.GetProposalId())
+	}
+	if !bytes.Equal(o2.In.GetProposalId(), []byte{35}) {
+		t.Error("OrphanList insert error4!", "id", o1.In.GetProposalId())
+	}
+	orphan4 := &storage.ProposalNode{
+		In: MockCreateQC([]byte{10}, 1, []byte{0}, 0),
+	}
+	tree.UpdateQcStatus(orphan4)
+	if orphan.Len() != 0 {
+		t.Error("OrphanList adopt error!")
+	}
+}

kernel/consensus/base/driver/chained-bft/pacemaker.go
@@ -1,5 +1,9 @@
 package chained_bft
 
+import (
+	"github.com/superconsensus/matrixcore/kernel/consensus/base/driver/chained-bft/storage"
+)
+
 // PacemakerInterface is the interface of Pacemaker. It responsible for generating a new round.
 // We assume Pacemaker in all correct replicas will have synchronized leadership after GST.
 // Safty is entirely decoupled from liveness by any potential instantiation of Packmaker.
@@ -8,7 +12,7 @@ type PacemakerInterface interface {
 	// CurrentView return current view of this node.
 	GetCurrentView() int64
 	// 原NextNewProposal，generate new proposal directly.
-	AdvanceView(qc QuorumCertInterface) (bool, error)
+	AdvanceView(qc storage.QuorumCertInterface) (bool, error)
 }
 
 // DefaultPaceMaker 是一个PacemakerInterface的默认实现，我们与PacemakerInterface放置在一起，方便查看
@@ -20,7 +24,7 @@ type DefaultPaceMaker struct {
 	// timeout int64
 }
 
-func (p *DefaultPaceMaker) AdvanceView(qc QuorumCertInterface) (bool, error) {
+func (p *DefaultPaceMaker) AdvanceView(qc storage.QuorumCertInterface) (bool, error) {
 	r := qc.GetProposalView()
 	if r+1 > p.CurrentView {
 		p.CurrentView = r + 1

kernel/consensus/base/driver/chained-bft/pacemaker_test.go
@@ -2,14 +2,16 @@ package chained_bft
 
 import (
 	"testing"
+
+	"github.com/superconsensus/matrixcore/kernel/consensus/base/driver/chained-bft/storage"
 )
 
 func TestPaceMaker(t *testing.T) {
 	p := &DefaultPaceMaker{
 		CurrentView: 0,
 	}
-	qc := &QuorumCert{
-		VoteInfo: &VoteInfo{
+	qc := &storage.QuorumCert{
+		VoteInfo: &storage.VoteInfo{
 			ProposalId:   []byte{1},
 			ProposalView: 1,
 		},

kernel/consensus/base/driver/chained-bft/saftyrules_test.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 
 	cCrypto "github.com/superconsensus/matrixcore/kernel/consensus/base/driver/chained-bft/crypto"
+	"github.com/superconsensus/matrixcore/kernel/consensus/base/driver/chained-bft/mock"
 	chainedBftPb "github.com/superconsensus/matrixcore/kernel/consensus/base/driver/chained-bft/pb"
 )
 
@@ -46,42 +47,32 @@ func TestIsInSlice(t *testing.T) {
 }
 
 func TestCheckProposal(t *testing.T) {
+	th, _ := mock.NewTestHelper()
+	defer th.Close()
 	s := &DefaultSaftyRules{
 		lastVoteRound:  0,
 		preferredRound: 0,
-		QcTree:         initQcTree(),
-		Log:            NewFakeLogger("nodeA"),
+		QcTree:         mock.MockInitQcTree(),
+		Log:            th.Log,
 	}
 	a, cc := NewFakeCryptoClient("nodeA", t)
 	s.Crypto = &cCrypto.CBFTCrypto{
 		Address:      &a,
 		CryptoClient: cc,
 	}
-	generic := CreateQC([]byte{1}, 1, []byte{0}, 01)
+	generic := mock.MockCreateQC([]byte{1}, 1, []byte{0}, 01)
 	msg := &chainedBftPb.ProposalMsg{
 		ProposalView: 1,
 		ProposalId:   []byte{1},
 	}
-	r, err := s.Crypto.SignProposalMsg(msg)
-	if err != nil {
-		t.Error("SignProposalMsg error")
-		return
-	}
-	generic.SignInfos = []*chainedBftPb.QuorumCertSign{r.Sign}
-	node1 := &ProposalNode{
-		In: generic,
-	}
-	if err := s.QcTree.updateQcStatus(node1); err != nil {
+	r, _ := s.Crypto.SignProposalMsg(msg)
+	node1 := mock.MockCreateNode(generic, []*chainedBftPb.QuorumCertSign{r.Sign})
+	if err := s.QcTree.UpdateQcStatus(node1); err != nil {
 		t.Error("TestUpdateQcStatus empty parent error")
 		return
 	}
-	proposal := CreateQC([]byte{2}, 2, []byte{1}, 1)
-	proposal.VoteInfo.ParentId = []byte{1}
+	proposal := mock.MockCreateQC([]byte{2}, 2, []byte{1}, 1)
 	s.CheckProposal(proposal, generic, []string{"gNhga8vLc4JcmoHB2yeef2adBhntkc5d1"})
 	s.VoteProposal([]byte{2}, 2, generic)
 	s.CheckVote(generic, "123", []string{"gNhga8vLc4JcmoHB2yeef2adBhntkc5d1"})
-}
-
-func TestCheckVote(t *testing.T) {
-
 }

kernel/consensus/base/driver/chained-bft/storage/quorum_cert_impl.go
@@ -1,0 +1,35 @@
+package storage
+
+import (
+	pb "github.com/superconsensus/matrixcore/kernel/consensus/base/driver/chained-bft/pb"
+)
+
+// quorumCert 是HotStuff的基础结构，它表示了一个节点本地状态以及其余节点对该状态的确认
+type QuorumCert struct {
+	// 本次qc的vote对象，该对象中嵌入了上次的QCid，因此删除原有的ProposalMsg部分
+	VoteInfo *VoteInfo
+	// 当前本地账本的状态
+	LedgerCommitInfo *LedgerCommitInfo
+	// SignInfos is the signs of the leader gathered from replicas of a specifically certType.
+	SignInfos []*pb.QuorumCertSign
+}
+
+func (qc *QuorumCert) GetProposalView() int64 {
+	return qc.VoteInfo.ProposalView
+}
+
+func (qc *QuorumCert) GetProposalId() []byte {
+	return qc.VoteInfo.ProposalId
+}
+
+func (qc *QuorumCert) GetParentProposalId() []byte {
+	return qc.VoteInfo.ParentId
+}
+
+func (qc *QuorumCert) GetParentView() int64 {
+	return qc.VoteInfo.ParentView
+}
+
+func (qc *QuorumCert) GetSignsInfo() []*pb.QuorumCertSign {
+	return qc.SignInfos
+}
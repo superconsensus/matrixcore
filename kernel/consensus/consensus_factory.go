OpenAtomFoundation/xupercore

fix consensus upgrade bugs (#312)

kernel/consensus/consensus_factory.go
@@ -1,0 +1,36 @@
+package consensus
+
+import (
+	cctx "github.com/xuperchain/xupercore/kernel/consensus/context"
+	"github.com/xuperchain/xupercore/kernel/consensus/def"
+)
+
+var consensusMap = make(map[string]NewStepConsensus)
+
+type NewStepConsensus func(cCtx cctx.ConsensusCtx, cCfg def.ConsensusConfig) ConsensusImplInterface
+
+// Register 不同类型的共识需要提前完成注册
+func Register(name string, f NewStepConsensus) error {
+	if f == nil {
+		panic("Pluggable Consensus::Register::new function is nil")
+	}
+	if _, dup := consensusMap[name]; dup {
+		panic("Pluggable Consensus::Register::called twice for func " + name)
+	}
+	consensusMap[name] = f
+	return nil
+}
+
+// NewPluginConsensus 新建可插拔共识实例
+func NewPluginConsensus(cCtx cctx.ConsensusCtx, cCfg def.ConsensusConfig) (ConsensusImplInterface, error) {
+	if cCfg.ConsensusName == "" {
+		return nil, EmptyConsensusName
+	}
+	if cCfg.StartHeight < 0 {
+		return nil, BeginBlockIdErr
+	}
+	if f, ok := consensusMap[cCfg.ConsensusName]; ok {
+		return f(cCtx, cCfg), nil
+	}
+	return nil, ConsensusNotRegister
+}

kernel/engines/xuperos/reader/consensus_info.go
@@ -2,14 +2,14 @@ package reader
 
 import (
 	xctx "github.com/xuperchain/xupercore/kernel/common/xcontext"
-	consBase "github.com/xuperchain/xupercore/kernel/consensus/base"
+	cons "github.com/xuperchain/xupercore/kernel/consensus"
 	"github.com/xuperchain/xupercore/kernel/engines/xuperos/common"
 	"github.com/xuperchain/xupercore/lib/logs"
 )
 
 type ConsensusReader interface {
 	// 获取共识状态
-	GetConsStatus() (consBase.ConsensusStatus, error)
+	GetConsStatus() (cons.ConsensusStatus, error)
 	// 共识特定共识类型的操作后续统一通过合约操作
 	// tdpos目前已经提供的rpc接口，看是否有业务依赖
 	// 视情况决定是不是需要继续支持，需要支持走代理合约调用
@@ -35,7 +35,7 @@ func NewConsensusReader(chainCtx *common.ChainCtx, baseCtx xctx.XContext) Consen
 	return reader
 }
 
-func (t *consensusReader) GetConsStatus() (consBase.ConsensusStatus, error) {
+func (t *consensusReader) GetConsStatus() (cons.ConsensusStatus, error) {
 	cons, _ := t.chainCtx.Consensus.GetConsensusStatus()
 	return cons, nil
 }

superconsensus/matrixcore

(cherry picked from commit 2954977391612b2b7946900c1781386e2ac07c7d)

kernel/consensus/consensus_factory.go
@@ -1,0 +1,36 @@
+package consensus
+
+import (
+	cctx "github.com/superconsensus/matrixcore/kernel/consensus/context"
+	"github.com/superconsensus/matrixcore/kernel/consensus/def"
+)
+
+var consensusMap = make(map[string]NewStepConsensus)
+
+type NewStepConsensus func(cCtx cctx.ConsensusCtx, cCfg def.ConsensusConfig) ConsensusImplInterface
+
+// Register 不同类型的共识需要提前完成注册
+func Register(name string, f NewStepConsensus) error {
+	if f == nil {
+		panic("Pluggable Consensus::Register::new function is nil")
+	}
+	if _, dup := consensusMap[name]; dup {
+		panic("Pluggable Consensus::Register::called twice for func " + name)
+	}
+	consensusMap[name] = f
+	return nil
+}
+
+// NewPluginConsensus 新建可插拔共识实例
+func NewPluginConsensus(cCtx cctx.ConsensusCtx, cCfg def.ConsensusConfig) (ConsensusImplInterface, error) {
+	if cCfg.ConsensusName == "" {
+		return nil, EmptyConsensusName
+	}
+	if cCfg.StartHeight < 0 {
+		return nil, BeginBlockIdErr
+	}
+	if f, ok := consensusMap[cCfg.ConsensusName]; ok {
+		return f(cCtx, cCfg), nil
+	}
+	return nil, ConsensusNotRegister
+}

kernel/engines/xuperos/reader/consensus_info.go
@@ -2,14 +2,14 @@ package reader
 
 import (
 	xctx "github.com/superconsensus/matrixcore/kernel/common/xcontext"
-	consBase "github.com/superconsensus/matrixcore/kernel/consensus/base"
+	cons "github.com/superconsensus/matrixcore/kernel/consensus"
 	"github.com/superconsensus/matrixcore/kernel/engines/xuperos/common"
 	"github.com/superconsensus/matrixcore/lib/logs"
 )
 
 type ConsensusReader interface {
 	// 获取共识状态
-	GetConsStatus() (consBase.ConsensusStatus, error)
+	GetConsStatus() (cons.ConsensusStatus, error)
 	// 共识特定共识类型的操作后续统一通过合约操作
 	// tdpos目前已经提供的rpc接口，看是否有业务依赖
 	// 视情况决定是不是需要继续支持，需要支持走代理合约调用
@@ -35,7 +35,7 @@ func NewConsensusReader(chainCtx *common.ChainCtx, baseCtx xctx.XContext) Consen
 	return reader
 }
 
-func (t *consensusReader) GetConsStatus() (consBase.ConsensusStatus, error) {
+func (t *consensusReader) GetConsStatus() (cons.ConsensusStatus, error) {
 	cons, _ := t.chainCtx.Consensus.GetConsensusStatus()
 	return cons, nil
 }
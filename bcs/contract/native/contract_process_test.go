OpenAtomFoundation/xupercore

Fail fast when java runtime not found in java native contract (#205)

bcs/contract/native/contract_process_test.go
@@ -1,0 +1,81 @@
+package native
+
+import (
+	"github.com/xuperchain/xupercore/kernel/contract"
+	"github.com/xuperchain/xupercore/protos"
+	"os/exec"
+	"testing"
+	"time"
+)
+
+func TestCommandNotFound(t *testing.T) {
+
+	t.Run("testDocker", func(t *testing.T) {
+		if resp, err := exec.Command("docker", "info").CombinedOutput(); err != nil {
+			t.Skip(string(resp))
+		}
+		pm, err := newContractProcess(&contract.NativeConfig{
+			Driver:      "native",
+			StopTimeout: 5,
+			Enable:      true,
+			Docker: contract.NativeDockerConfig{
+				Enable:    true,
+				ImageName: "ubuntu:18.04:",
+				Cpus:      1,
+				Memory:    "1G",
+			},
+		}, "xchain-test", "/tmp", "", &protos.WasmCodeDesc{
+			Runtime: "java",
+			Digest:  []byte("nativetest"),
+		})
+
+		if err != nil {
+			t.Error(err)
+		}
+
+		process, err := pm.makeNativeProcess()
+
+		err = process.Start()
+		defer process.Stop(time.Second)
+		if err == nil {
+			t.Error("expect error,get nil")
+		}
+		t.Log(err)
+	})
+
+	t.Run("testDockerOpenJDK", func(t *testing.T) {
+		if resp, err := exec.Command("docker", "info").CombinedOutput(); err != nil {
+			t.Skip(string(resp))
+		}
+		cp, err := newContractProcess(&contract.NativeConfig{
+			Driver:      "native",
+			StopTimeout: 5,
+			Enable:      true,
+			Docker: contract.NativeDockerConfig{
+				Enable:    true,
+				ImageName: "openjdk:8u292-slim-buster",
+				Cpus:      1,
+				Memory:    "1G",
+			},
+		}, "xchain-test", "/tmp", "", &protos.WasmCodeDesc{
+			Runtime: "java",
+			Digest:  []byte("nativetest"),
+		})
+		process, err := cp.makeNativeProcess()
+		if err != nil {
+			t.Error(err)
+		}
+
+		defer process.Stop(time.Second)
+
+		if err != nil {
+			t.Error(err)
+		}
+
+		err = process.Start()
+		if err != nil {
+			t.Error(err)
+		}
+		//t.Log(err)
+	})
+}

superconsensus/matrixcore

(cherry picked from commit 9bdaaa21ad0a8c53efd3dd8d961663f67c107d80)

bcs/contract/native/contract_process_test.go
@@ -1,0 +1,81 @@
+package native
+
+import (
+	"github.com/superconsensus/matrixcore/kernel/contract"
+	"github.com/superconsensus/matrixcore/protos"
+	"os/exec"
+	"testing"
+	"time"
+)
+
+func TestCommandNotFound(t *testing.T) {
+
+	t.Run("testDocker", func(t *testing.T) {
+		if resp, err := exec.Command("docker", "info").CombinedOutput(); err != nil {
+			t.Skip(string(resp))
+		}
+		pm, err := newContractProcess(&contract.NativeConfig{
+			Driver:      "native",
+			StopTimeout: 5,
+			Enable:      true,
+			Docker: contract.NativeDockerConfig{
+				Enable:    true,
+				ImageName: "ubuntu:18.04:",
+				Cpus:      1,
+				Memory:    "1G",
+			},
+		}, "xchain-test", "/tmp", "", &protos.WasmCodeDesc{
+			Runtime: "java",
+			Digest:  []byte("nativetest"),
+		})
+
+		if err != nil {
+			t.Error(err)
+		}
+
+		process, err := pm.makeNativeProcess()
+
+		err = process.Start()
+		defer process.Stop(time.Second)
+		if err == nil {
+			t.Error("expect error,get nil")
+		}
+		t.Log(err)
+	})
+
+	t.Run("testDockerOpenJDK", func(t *testing.T) {
+		if resp, err := exec.Command("docker", "info").CombinedOutput(); err != nil {
+			t.Skip(string(resp))
+		}
+		cp, err := newContractProcess(&contract.NativeConfig{
+			Driver:      "native",
+			StopTimeout: 5,
+			Enable:      true,
+			Docker: contract.NativeDockerConfig{
+				Enable:    true,
+				ImageName: "openjdk:8u292-slim-buster",
+				Cpus:      1,
+				Memory:    "1G",
+			},
+		}, "xchain-test", "/tmp", "", &protos.WasmCodeDesc{
+			Runtime: "java",
+			Digest:  []byte("nativetest"),
+		})
+		process, err := cp.makeNativeProcess()
+		if err != nil {
+			t.Error(err)
+		}
+
+		defer process.Stop(time.Second)
+
+		if err != nil {
+			t.Error(err)
+		}
+
+		err = process.Start()
+		if err != nil {
+			t.Error(err)
+		}
+		//t.Log(err)
+	})
+}